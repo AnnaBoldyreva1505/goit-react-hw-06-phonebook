11afe7d7e47ea48b7e05fcb907bc43f98cef4d02
AnnaBoldyreva1505 <hannabl1988@gmail.com>
2023-06-16 00:52:23 +0600

    add filter

---
+++ src/redux/filterSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  filter: '',
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterContacts(state, action) {
+      state.status = action.payload;
+    },
+  },
+});
+
+console.log(filterSlice)
+
+export const filterReducer = filterSlice.reducer;
+export const { filterContacts } = filterSlice.actions;
